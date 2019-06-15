1d9116420cd2543e62d41c0f39b060b0e82d1adc
sanjay chaudhary <snjy991@gmail.com>
2019-06-15 18:39:50 +0530

    commited

--- JenkinsPipelines/DeployCloudHub.groovy
+++ JenkinsPipelines/DeployCloudHub.groovy
@@ -1,7 +1,7 @@
 
 def deployToCloudHub(String JarName){
 	
-	String PATH= "/devops/out/CDScript/working/"+JarName
+	String PATH= "/Users/snjy/Desktop/JenkinsPipelines"+JarName
 	println (PATH);
 	
 	Properties properties = new Properties()
@@ -11,7 +11,10 @@ def deployToCloudHub(String JarName){
 
 	if(env.DeploymentType == "FirstRun")
 	{
-		println("first time deploymnent")
+		withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId:'anypoint-studio-snjy', usernameVariable: 'Username', passwordVariable: 'Password']]) {
+                   // jarName=sh (script : 'ls /devops/out/CDScript/working/${BUILD_NUMBER}_${JOB_NAME}/',returnStdout:true)
+                    println "jar name ${jarName}"
+                    sh """anypoint-cli --username ${Username} --password ${Password} runtime-mgr cloudhub-application deploy --runtime properties.runtime --workers properties.worker --workerSize properties.workerSize --region properties.region --property "username:${Username}" "muleappfirstproject" ${PATH} """
 	}
 	else{
 		println("general deploymnent")
