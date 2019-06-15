d78ce2b1d86187e8d54f43e8c208f16245512bf6
sanjay chaudhary <snjy991@gmail.com>
2019-06-15 17:02:52 +0530

    Update Common.groovy

--- JenkinsPipelines/Common.groovy
+++ JenkinsPipelines/Common.groovy
@@ -1,4 +1,7 @@
 def testMethod(){
 	println("Hey I am here");
+	withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId:'ANYPOINT', usernameVariable: 'Username', passwordVariable: 'Password']]) {
+		println(Username);	
+	}	
 }
 return this
